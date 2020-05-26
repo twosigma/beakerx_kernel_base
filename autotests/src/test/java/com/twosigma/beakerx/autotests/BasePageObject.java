373ab0c14b733d1c15bdc575a53f341ae664e103
vladimir efimov <vefimov@qulix.com>
2020-05-26 22:09:39 +0300

    clean code

--- autotests/src/test/java/com/twosigma/beakerx/autotests/BasePageObject.java
+++ autotests/src/test/java/com/twosigma/beakerx/autotests/BasePageObject.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2020 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.twosigma.beakerx.autotests;
 
 import org.openqa.selenium.By;
@@ -43,13 +59,8 @@ public abstract class BasePageObject {
     public abstract By getAllOutputsStderrSelector();
 
     public void doubleClick(By locator){
-        //Double click the button to launch an alertbox
-//        Actions action = new Actions(webDriver);
         WebElement element = webDriver.findElement(locator);
-//        System.out.println("---------------------------------------");
-//        System.out.println(element.getAttribute("innerHTML"));
         action.moveToElement(element).doubleClick().pause(500).perform();
-//        action.doubleClick(element).perform();
     }
 
     public void pause(int mseconds){
--- autotests/src/test/java/com/twosigma/beakerx/autotests/BaseTest.java
+++ autotests/src/test/java/com/twosigma/beakerx/autotests/BaseTest.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2020 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.twosigma.beakerx.autotests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,18 +32,16 @@ public class BaseTest {
     public static void setupClass() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        // Tested in Google Chrome 59 on Linux. More info on:
-        // https://developers.google.com/web/updates/2017/04/headless-chrome
 //    options.addArguments("--headless");
 //    options.addArguments("--disable-gpu");
         options.addArguments("window-size=1920,1080");
         driver = new ChromeDriver(options);
         if("lab".equalsIgnoreCase(System.getProperty("cur_app"))){
-            System.out.println("-------------------lab-------------------");
+            System.out.println("current app is jupyter lab");
             beakerxPO = new LabPO(driver);
         }
         else {
-            System.out.println("-------------------nb-------------------");
+            System.out.println("current app is jupyter notebook");
             beakerxPO = new NotebookPO(driver);
         }
     }
@@ -38,7 +52,6 @@ public class BaseTest {
 
     @AfterClass
     public static void teardownClass() {
-        System.out.println("------------ close and halt notebook -------------------------------");
         beakerxPO.closeAndHaltNotebook();
         if (driver != null) {
             driver.quit();
